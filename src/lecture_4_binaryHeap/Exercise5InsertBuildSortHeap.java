d2989c7b9b163957154495298e60e6350f3f68d3
AleksanderGD <agdbusiness@hotmail.com>
2020-09-27 21:53:44 +0200

    todo exercise 5

---
+++ src/lecture_4_binaryHeap/Exercise5InsertBuildSortHeap.java
@@ -0,0 +1,16 @@
+package lecture_4_binaryHeap;
+
+/**
+ * Write a program to take N elements and do the following:
+ * 1. Insert them into a heap one by one.
+ * 2. Build a heap in linear time.
+ * Compare the running time of both algorithms for sorted, reverse-ordered, and
+ * random inputs
+ */
+public class Exercise5InsertBuildSortHeap {
+    public static void main(String[] args) {
+
+    }
+
+
+}
